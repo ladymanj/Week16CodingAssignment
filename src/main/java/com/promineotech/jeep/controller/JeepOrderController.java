11bbb499a0c5740e38ecaf68d4bf8fec03a38451
Jordan Ladyman <jordan.ladyman@outlook.com>
2022-06-17 22:22:30 -0500

    fixed import statement

--- src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
+++ src/main/java/com/promineotech/jeep/controller/JeepOrderController.java
@@ -6,6 +6,7 @@ package com.promineotech.jeep.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import com.promineotech.jeep.entity.Order;
@@ -14,7 +15,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 /**
